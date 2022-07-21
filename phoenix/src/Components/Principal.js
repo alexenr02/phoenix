c5b5ad08045e7f7141a8285bc422e266683df906
Carla Corona <a01023339@itesm.mx>
2022-07-21 02:38:56 -0500

    Pagina Principal V1
    Modificaciones a la pagina principal, primer avance de esta seccion

--- phoenix/src/Components/Principal.js
+++ phoenix/src/Components/Principal.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import NavBar from './NavBar';
+import mainmedia from "../imgs/mainmedia.png";
 
 function Principal() {
   return (
     <div>
-      <p>Principal</p>
+      <section id="IniPrincipal">
+        <h1> Tienda en Linea</h1>
+      <p>Proyecto Final DAW Equipo 9</p>
+      </section>
+      <img id="mainmedia"src={mainmedia} alt="Promo image"/>
 
+          <h2>Mision</h2>
+          <p>Proyecto Final Desarrollo de Aplicaciones Web Verano 2022</p>
+
+          <h2>Vision</h2>
+          <p>Creacion de una pagina de tienda en linea navegable donde se puedan agregar productos a un carrito </p>
+      <section id="IniPrincipal">
+        <h3> Integrantes: </h3>
+      <p>Carla Corona Cardoso,José Antonio Hernández Cruz,Luis Alejandro Enríquez Solís</p>
+      </section>
     </div>
   )
 }
