777c4075679893d4ae890acad11a42d128e775c8
Jose6464Cruz <37162809+Jose6464Cruz@users.noreply.github.com>
2022-07-22 17:43:12 -0500

    Barra desplegable
    En esta version la NavBar ya despliega correctamente

--- phoenix/src/Components/NavBar.js
+++ phoenix/src/Components/NavBar.js
@@ -1,27 +1,33 @@
+
+
 import {React, useState} from 'react'
 
 function NavBar() {
-  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="navigationbar">
-<a class="navbar-brand" href="/">Phoenix</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => {
-          setIsNavExpanded(!isNavExpanded);
-        }}>
-    <span class="navbar-toggler-icon"></span>
+
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse} >
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div className={
-          isNavExpanded ? "collapse navbar-collapse expanded" : "collapse navbar-collapse" 
-        } id="navbarNav">
-    <ul className="navbar-nav">
-    <li className="nav-item">
-        <a class="nav-link" href="/">Principal</a>
+  
+  <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarsExample09" >
+    <p className="navbar-brand" >Phoenix</p>
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item ">
+        <a className="nav-link" href="/">Principal</a>
       </li>
-      <li className="nav-item active">
-        <a className="nav-link" href="/productos">Productos<span class="sr-only">(current)</span></a>
+      <li className="nav-item ">
+        <a className="nav-link" href="/Productos">Productos</a>
       </li>
-      <li className="nav-item">
-        <a class="nav-link" href="/carrito">Carrito</a>
+      <li className="nav-item ">
+        <a className="nav-link " href="/carrito">Carrito</a>
+
+
       </li>
     </ul>
   </div>
