f33e6624738aa4c75a04e6066d9d3d899298f393
Alejandro Enriquez <aelejandro9@gmail.com>
2022-07-27 22:14:49 -0500

    base de datos funcionando correctamente

--- phoenix/src/zapatos.js
+++ phoenix/src/zapatos.js
@@ -25,9 +25,6 @@ app.use(express.json());
 app.use(cors())
 
 
-//var carrito=[  
-
-//];
 
 // Routes
   app.get("/", function(req, res) {
@@ -54,13 +51,6 @@ app.get("/api/carrito/:model", async function(req, res) {
   
     console.log(chosen);
   
-    /*for (var i = 0; i < carrito.length; i++) {
-      if (chosen === carrito[i].modelo) {
-        return res.json(carrito[i]);
-      }
-    }
-  
-    return res.send("No moedl found");*/
   
     const prods = await Product.findOne({routeName:chosen})
     if(prods)
@@ -85,7 +75,7 @@ app.get("/api/carrito/:model", async function(req, res) {
   
     newmodel.routeName = newmodel.modelo.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newmodel);
+    //console.log(newmodel);
     
     const char = new Product(newmodel)
     
@@ -108,15 +98,20 @@ app.get("/api/carrito/:model", async function(req, res) {
 
   // Erase model - takes in JSON input//
   app.delete("/api/carrito/del", async function(req, res) {
+    
     let delmodel = req.body.id;
+   
+    
+    try{
+        await Product.deleteOne({id:delmodel})
+        console.log("dato borrado correctamente")
+        res.status(200).json(delmodel);
+    
+      } catch(err){
+        console.log(err)
+      }
 
-    Product=Product.filter((prev) => prev.id !== delmodel);
-    
-    res.status(200).json(delmodel);
-    
   });
-
-
 
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
